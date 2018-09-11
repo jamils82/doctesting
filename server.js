88d25f4ce512e8f75466ae314af0f77800e61906
SAAD JAMIL <sjamils82@gmail.com>
2018-09-11 12:08:29 +0500

    sadad

--- server.js
+++ server.js
@@ -1,4 +1,20 @@
-var http = require('http');
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/<name-of-app>'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
+/*var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
@@ -21,7 +37,7 @@ app.use(function (req, res, next) {
  });  
   
 
-
+*/
 /*var express = require('express');  
 var path = require("path");   
 var bodyParser = require('body-parser');  
