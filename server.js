187957ef157fae3d2380b0e4b740796ab411086f
SAAD JAMIL <sjamils82@gmail.com>
2018-09-11 11:50:00 +0500

    initial commit

---
+++ server.js
@@ -0,0 +1,117 @@
+var http = require('http');
+
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('Hello World!'); //write a response to the client
+  res.end(); //end the response
+}).listen(8080); //the server object listens on port 8080
+
+var app = express()  
+app.use(bodyParser());  
+app.use(bodyParser.json({limit:'5mb'}));   
+app.use(bodyParser.urlencoded({extended:true}));  
+   
+  
+app.use(function (req, res, next) {        
+     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');    
+     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');    
+     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');      
+     res.setHeader('Access-Control-Allow-Credentials', true);       
+     next();  
+ });  
+  
+
+
+/*var express = require('express');  
+var path = require("path");   
+var bodyParser = require('body-parser');  
+var mongo = require("mongoose");  
+  
+var db = mongo.connect("mongodb://localhost:3000/AngularCRUD",{ useNewUrlParser: true }, function(err, response){  
+   if(err){ console.log( err); }  
+   else{ console.log('Connected to ' + db, ' + ', response); }  
+});  
+  
+   
+var app = express()  
+app.use(bodyParser());  
+app.use(bodyParser.json({limit:'5mb'}));   
+app.use(bodyParser.urlencoded({extended:true}));  
+   
+  
+app.use(function (req, res, next) {        
+     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');    
+     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');    
+     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');      
+     res.setHeader('Access-Control-Allow-Credentials', true);       
+     next();  
+ });  
+  
+ var Schema = mongo.Schema;  
+  
+var UsersSchema = new Schema({      
+ name: { type: String   },       
+ address: { type: String   },   
+},{ versionKey: false });  
+   
+  
+var model = mongo.model('users', UsersSchema, 'users');  
+  
+app.post("/api/SaveUser",function(req,res){   
+ var mod = new model(req.body);  
+ if(req.body.mode =="Save")  
+ {  
+    mod.save(function(err,data){  
+      if(err){  
+         res.send(err);                
+      }  
+      else{        
+          res.send({data:"Record has been Inserted..!!"});  
+      }  
+ });  
+}  
+else   
+{  
+ model.findByIdAndUpdate(req.body.id, { name: req.body.name, address: req.body.address},  
+   function(err,data) {  
+   if (err) {  
+   res.send(err);         
+   }  
+   else{        
+          res.send({data:"Record has been Updated..!!"});  
+     }  
+ });  
+  
+  
+}  
+ })  
+  
+ app.post("/api/deleteUser",function(req,res){      
+    model.remove({ _id: req.body.id }, function(err) {    
+     if(err){    
+         res.send(err);    
+     }    
+     else{      
+            res.send({data:"Record has been Deleted..!!"});               
+        }    
+ });    
+   })  
+  
+  
+  
+ app.get("/api/getUser",function(req,res){  
+    model.find({},function(err,data){  
+              if(err){  
+                  res.send(err);  
+              }  
+              else{                
+                  res.send(data);  
+                  }  
+          });  
+  })  
+  
+  
+app.listen(8080, function () {  
+    
+ console.log('Example app listening on port 8080!')  
+})  */
