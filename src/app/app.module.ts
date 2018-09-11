aac8b818ee7e5d5f149f0c93a4073af406b1e4fc
jamils82 <sjamils82@gmail.com>
2018-09-11 20:02:28 +0500

    asdad

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+// import { MatFormFieldModule, MatInputModule } from '@angular/material';
 import { AppComponent } from './app.component';
-
+import { PublisherComponent } from './publisher/publisher.component';
+import { SubscriberComponent } from './subscriber/subscriber.component';
+import { OpentokService } from './opentok.service';
+import { CallComponent } from './call/call.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule, HttpClient , HttpHandler } from '@angular/common/http';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PublisherComponent,
+    SubscriberComponent,
+    CallComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+  //  MatFormFieldModule,
+  //  MatInputModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    OpentokService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
