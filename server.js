6488a57d8d82aeb4b6fd728ad510dc4d1ce8aa12
jamils82 <sjamils82@gmail.com>
2018-09-12 00:22:26 +0500

    dasd

--- server.js
+++ server.js
@@ -4,26 +4,17 @@ const path = require('path');
 
 const app = express();
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/<name-of-app>'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'<name-of-app>/src/index.html'));
+res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 5000 , function () {
     console.log(process.env.PORT || 5000);
 } );
-
-
-app.use(function (req, res, next) {        
-    res.setHeader('Access-Control-Allow-Origin', 'https://doctesting.herokuapp.com/');    
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');    
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');      
-    res.setHeader('Access-Control-Allow-Credentials', true);       
-    next();  
-});  
 /*var http = require('http');
 
 //create a server object:
