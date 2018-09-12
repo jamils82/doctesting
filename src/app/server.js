0e842154fc6992fba25c2795c9cb3c117bdb7f07
jamils82 <sjamils82@gmail.com>
2018-09-12 14:20:10 +0500

    ASDSD

--- src/app/server.js
+++ src/app/server.js
@@ -8,7 +8,7 @@ app.use(express.static(__dirname  ));
 
 app.get('/*', function(req,res) {
 
-    res.sendFile(path.join(__dirname, '/src/index.html'));
+    res.sendFile(path.join(__dirname));
 });
 
 // Start the app by listening on the default Heroku port
