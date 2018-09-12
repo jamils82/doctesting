e5a532f6e3cbdc97a20788c4eab2d7027e39997b
jamils82 <sjamils82@gmail.com>
2018-09-12 12:25:10 +0500

    ASDDASD

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static(__dirname + '/dist'));
 
 app.get('/app/src', function(req,res) {
     
-res.sendFile(path.join(__dirname, '/src/index.html'));
+res.sendFile(path.join(__dirname, '/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 5000 , function () {
