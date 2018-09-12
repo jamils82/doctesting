762a3d26ebd980cf6aef8a4f12efe74fce7509fb
jamils82 <sjamils82@gmail.com>
2018-09-12 18:34:30 +0500

    ASDSD

--- server.js
+++ server.js
@@ -5,12 +5,6 @@ const path = require('path');
 const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname ));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname, '/src' + '/index.html'));
-});
-
 app.get('/', function(req,res) {
     
 res.sendFile(path.join(__dirname, '/src' + '/index.html'));
