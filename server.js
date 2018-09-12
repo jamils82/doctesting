18d16ef736f9d1bce6185986e451195070388193
jamils82 <sjamils82@gmail.com>
2018-09-12 18:50:09 +0500

    ASDSD

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ app.get('/', function(req,res) {
 res.sendFile(path.join(__dirname, '/src' + '/index.html'));
 });
 app.get('', function(req,res) {
+    res.sendFile(path.join(__dirname, '/src' + '/index.html'));
+    });
     
+app.get('*', function(req,res) {
     res.sendFile(path.join(__dirname, '/src' + '/index.html'));
     });
 // Start the app by listening on the default Heroku port
