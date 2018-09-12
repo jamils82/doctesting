58a8129fc9cff92f81f5dfb17e939586f50e8e16
jamils82 <sjamils82@gmail.com>
2018-09-12 14:05:34 +0500

    ASDSD

--- src/app/server.js
+++ src/app/server.js
@@ -7,7 +7,8 @@ const app = express();
 app.use(express.static(__dirname , '/dist' ));
 
 app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname, '/doccall/index.html'));
+
+    res.sendFile(path.join(__dirname, '/<app-name>/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
