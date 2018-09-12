02dbfba12dd517cf918bc96c1d9291d8c2448056
jamils82 <sjamils82@gmail.com>
2018-09-12 13:25:01 +0500

    ASDSD

--- src/app/server.js
+++ src/app/server.js
@@ -4,11 +4,11 @@ const path = require('path');
 
 const app = express();
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname ,'/src' ));
+app.use(express.static(__dirname  ));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'app' + '/index.html'));
+res.sendFile(path.join(__dirname,'src' + 'app' + '/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
